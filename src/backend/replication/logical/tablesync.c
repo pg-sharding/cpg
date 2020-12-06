f3f210b6c711007ff4d1adf4a66439b1121b97bd
Andrey Borodin <amborodin@acm.org>
2020-12-06 10:01:59 +0500

    MDB replication role patch
    MDB replication role regression tests
    
    Patch allows user with mdb_replication role to use
    pg_create_logical_replication_slot, pg_replication_slot_advance,
    pg_drop_replication_slot functions to manage logical replication slots.
    Also, users with mdb_admin (which is memner of pg_create_subscription)
    can create subscribptions. Slot names starting with MDB.* are forbidden.
    
    Add run as owner tap tests
    
    More test cases in mdb_102
    
    Fix test
    
    Fix tests once again
    
    Never check for superuser in walsender

--- src/backend/replication/logical/tablesync.c
+++ src/backend/replication/logical/tablesync.c
@@ -122,6 +122,7 @@
 #include "utils/snapmgr.h"
 #include "utils/syscache.h"
 #include "utils/usercontext.h"
+#include "utils/acl.h"
 
 typedef enum
 {
@@ -1152,6 +1153,7 @@ copy_table(Relation rel)
 	ParseState *pstate;
 	List	   *options = NIL;
 	bool		gencol_published = false;
+	AclResult   aclresult;
 
 	/* Get the publisher relation info. */
 	fetch_remote_table_info(get_namespace_name(RelationGetNamespace(rel)),
@@ -1164,6 +1166,11 @@ copy_table(Relation rel)
 	/* Map the publisher relation to local one. */
 	relmapentry = logicalrep_rel_open(lrel.remoteid, NoLock);
 	Assert(rel == relmapentry->localrel);
+
+	/* Check permission on table. */
+	aclresult = pg_class_aclcheck(RelationGetRelid(rel), GetUserId(), ACL_INSERT);
+	if (aclresult != ACLCHECK_OK)
+		aclcheck_error(aclresult, get_relkind_objtype(rel->rd_rel->relkind), RelationGetRelationName(rel));
 
 	/* Start copy on the publisher. */
 	initStringInfo(&cmd);
