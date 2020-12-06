bc02c0975b77d6de37f028a1609145132f95fd30
Andrey Borodin <amborodin@acm.org>
2020-12-06 10:01:19 +0500

    Debian folder
    Add pg_version.sh
    
    remove pg version & changelog generation
    
    remove stip version
    
    Use mirror.yandex.ru instead of archives.ubuntu.com
    
    mdb_replication role patch

--- src/backend/replication/slotfuncs.c
+++ src/backend/replication/slotfuncs.c
@@ -24,6 +24,7 @@
 #include "utils/inval.h"
 #include "utils/pg_lsn.h"
 #include "utils/resowner.h"
+#include "utils/acl.h"
 
 static void
 check_permissions(void)
@@ -32,6 +33,33 @@ check_permissions(void)
 		ereport(ERROR,
 				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
 				 errmsg("must be superuser or replication role to use replication slots")));
+}
+
+static void
+check_mdb_replication(void)
+{
+	Oid         role;
+	role = get_role_oid("mdb_replication", true);
+	if (!superuser() && !has_rolreplication(GetUserId()) && !is_member_of_role(GetUserId(), role))
+		ereport(ERROR,
+				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+				 (errmsg("must be superuser, replication role or mdb_replication to use replication slots"))));
+}
+
+static void
+check_mdb_reserved_name(const char *name)
+{
+	/* ugly coding for speed (taken from IsReservedName) */
+	if (name[0] == 'm' &&
+			name[1] == 'd' &&
+			name[2] == 'b' &&
+		!superuser() && !has_rolreplication(GetUserId()))
+	{
+		ereport(ERROR,
+				(errcode(ERRCODE_RESERVED_NAME),
+				errmsg("slot name \"%s\" is reserved", name),
+				errdetail("Slot names starting with \"mdb\" are reserved.")));
+	}
 }
 
 /*
@@ -188,7 +216,8 @@ pg_create_logical_replication_slot(PG_FUNCTION_ARGS)
 	if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
 		elog(ERROR, "return type must be a row type");
 
-	check_permissions();
+	check_mdb_reserved_name(NameStr(*name));
+	check_mdb_replication();
 
 	CheckLogicalDecodingRequirements();
 
@@ -224,7 +253,9 @@ pg_drop_replication_slot(PG_FUNCTION_ARGS)
 {
 	Name		name = PG_GETARG_NAME(0);
 
-	check_permissions();
+	check_mdb_reserved_name(NameStr(*name));
+
+	check_mdb_replication();
 
 	CheckSlotRequirements();
 
