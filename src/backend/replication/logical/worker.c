3d8aab7b75e069bbe28b156c28fe2ad8fe9d6839
Andrey Borodin <amborodin@acm.org>
2020-12-06 10:01:59 +0500

    MDB replication role patch MDB replication role regression tests
    Patch allows user with mdb_replication role to use
    pg_create_logical_replication_slot, pg_replication_slot_advance,
    pg_drop_replication_slot functions to manage logical replication slots.
    Also, users with mdb_admin (which is memner of pg_create_subscription)
    can create subscribptions. Slot names starting with MDB.* are forbidden.
    
    Add run as owner tap tests
    
    More test cases in mdb_102

--- src/backend/replication/logical/worker.c
+++ src/backend/replication/logical/worker.c
@@ -209,6 +209,7 @@
 #include "utils/syscache.h"
 #include "utils/timeout.h"
 #include "utils/usercontext.h"
+#include "utils/acl.h"
 
 #define NAPTIME_PER_CYCLE 1000	/* max sleep time between cycles (1s) */
 
@@ -2394,6 +2395,7 @@ apply_handle_insert(StringInfo s)
 	TupleTableSlot *remoteslot;
 	MemoryContext oldctx;
 	bool		run_as_owner;
+	AclResult	aclresult;
 
 	/*
 	 * Quick return if we are skipping data modification changes or handling
@@ -2428,6 +2430,11 @@ apply_handle_insert(StringInfo s)
 
 	/* Set relation for error callback */
 	apply_error_callback_arg.rel = rel;
+	aclresult = pg_class_aclcheck(RelationGetRelid(rel->localrel), GetUserId(),
+								ACL_INSERT);
+	if (aclresult != ACLCHECK_OK)
+		aclcheck_error(aclresult, get_relkind_objtype(rel->localrel->rd_rel->relkind),
+						RelationGetRelationName(rel->localrel));
 
 	/* Initialize the executor state. */
 	edata = create_edata_for_relation(rel);
@@ -2546,6 +2553,7 @@ apply_handle_update(StringInfo s)
 	RTEPermissionInfo *target_perminfo;
 	MemoryContext oldctx;
 	bool		run_as_owner;
+	AclResult	aclresult;
 
 	/*
 	 * Quick return if we are skipping data modification changes or handling
@@ -2584,6 +2592,14 @@ apply_handle_update(StringInfo s)
 	run_as_owner = MySubscription->runasowner;
 	if (!run_as_owner)
 		SwitchToUntrustedUser(rel->localrel->rd_rel->relowner, &ucxt);
+
+	/* MDB check acl on relation */
+	aclresult = pg_class_aclcheck(RelationGetRelid(rel->localrel), GetUserId(),
+								ACL_UPDATE);
+	if (aclresult != ACLCHECK_OK)
+		aclcheck_error(aclresult, get_relkind_objtype(rel->localrel->rd_rel->relkind),
+						RelationGetRelationName(rel->localrel));
+
 
 	/* Initialize the executor state. */
 	edata = create_edata_for_relation(rel);
@@ -2727,6 +2743,7 @@ apply_handle_delete(StringInfo s)
 	TupleTableSlot *remoteslot;
 	MemoryContext oldctx;
 	bool		run_as_owner;
+	AclResult	aclresult;
 
 	/*
 	 * Quick return if we are skipping data modification changes or handling
@@ -2753,7 +2770,6 @@ apply_handle_delete(StringInfo s)
 
 	/* Set relation for error callback */
 	apply_error_callback_arg.rel = rel;
-
 	/* Check if we can do the delete. */
 	check_relation_updatable(rel);
 
@@ -2764,6 +2780,12 @@ apply_handle_delete(StringInfo s)
 	run_as_owner = MySubscription->runasowner;
 	if (!run_as_owner)
 		SwitchToUntrustedUser(rel->localrel->rd_rel->relowner, &ucxt);
+
+	aclresult = pg_class_aclcheck(RelationGetRelid(rel->localrel), GetUserId(),
+								ACL_DELETE);
+	if (aclresult != ACLCHECK_OK)
+		aclcheck_error(aclresult, get_relkind_objtype(rel->localrel->rd_rel->relkind),
+						RelationGetRelationName(rel->localrel));
 
 	/* Initialize the executor state. */
 	edata = create_edata_for_relation(rel);
@@ -3168,6 +3190,7 @@ apply_handle_truncate(StringInfo s)
 	List	   *relids_logged = NIL;
 	ListCell   *lc;
 	LOCKMODE	lockmode = AccessExclusiveLock;
+	AclResult	aclresult;
 
 	/*
 	 * Quick return if we are skipping data modification changes or handling
@@ -3196,6 +3219,12 @@ apply_handle_truncate(StringInfo s)
 			logicalrep_rel_close(rel, lockmode);
 			continue;
 		}
+
+		aclresult = pg_class_aclcheck(RelationGetRelid(rel->localrel), GetUserId(),
+								  ACL_TRUNCATE);
+		if (aclresult != ACLCHECK_OK)
+			aclcheck_error(aclresult, get_relkind_objtype(rel->localrel->rd_rel->relkind),
+							RelationGetRelationName(rel->localrel));
 
 		remote_rels = lappend(remote_rels, rel);
 		TargetPrivilegesCheck(rel->localrel, ACL_TRUNCATE);
