db360eb359f68f6537df08c9b7d4a743b19235b7
Andrey Borodin <amborodin@acm.org>
2020-12-06 10:01:59 +0500

    MDB replication role patch and  regression tests
    Patch allows user with mdb_replication role to use
    pg_create_logical_replication_slot, pg_replication_slot_advance,
    pg_drop_replication_slot functions to manage logical replication slots.
    Also, users with mdb_admin (which is memner of pg_create_subscription)
    can create subscribptions. Slot names starting with MDB.* are forbidden.

--- src/backend/replication/walsender.c
+++ src/backend/replication/walsender.c
@@ -114,6 +114,10 @@ WalSnd	   *MyWalSnd = NULL;
 
 /* Global state */
 bool		am_walsender = false;	/* Am I a walsender process? */
+
+bool		is_repl_role = false;	/* has replication privelege  */
+bool		is_mdb_repl_role = false;	/* mdb_replication member */
+
 bool		am_cascading_walsender = false; /* Am I cascading WAL to another
 											 * standby? */
 bool		am_db_walsender = false;	/* Connected to a database? */
@@ -206,6 +210,40 @@ typedef struct
 
 /* The size of our buffer of time samples. */
 #define LAG_TRACKER_BUFFER_SIZE 8192
+
+static void
+check_permissions(void)
+{
+	if (!is_repl_role && !superuser())
+		ereport(ERROR,
+				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+				 (errmsg("must be superuser or replication role to use replication slots"))));
+}
+
+static void
+check_mdb_replication(void)
+{
+	if (!is_repl_role && !is_mdb_repl_role && !superuser())
+		ereport(ERROR,
+				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+				 (errmsg("must be superuser or replication role to use replication slots"))));
+}
+
+static void
+check_mdb_reserved_name(const char *name)
+{
+	/* ugly coding for speed (taken from IsReservedName) */
+	if (name[0] == 'm' &&
+			name[1] == 'd' &&
+			name[2] == 'b' &&
+		!is_repl_role && !superuser())
+	{
+		ereport(ERROR,
+				(errcode(ERRCODE_RESERVED_NAME),
+				 errmsg("slot name \"%s\" is reserved", name),
+				 errdetail("Slot names starting with \"mdb\" are reserved.")));
+	}
+}
 
 /* A mechanism for tracking replication lag. */
 typedef struct
@@ -1077,6 +1115,7 @@ CreateReplicationSlot(CreateReplicationSlotCmd *cmd)
 
 	if (cmd->kind == REPLICATION_KIND_PHYSICAL)
 	{
+		check_permissions();
 		ReplicationSlotCreate(cmd->slotname, false,
 							  cmd->temporary ? RS_TEMPORARY : RS_PERSISTENT,
 							  false);
@@ -1084,6 +1123,8 @@ CreateReplicationSlot(CreateReplicationSlotCmd *cmd)
 	else
 	{
 		CheckLogicalDecodingRequirements();
+		check_mdb_replication();
+		check_mdb_reserved_name(cmd->slotname);
 
 		/*
 		 * Initially create persistent slot as ephemeral - that allows us to
@@ -1259,6 +1300,7 @@ CreateReplicationSlot(CreateReplicationSlotCmd *cmd)
 static void
 DropReplicationSlot(DropReplicationSlotCmd *cmd)
 {
+	check_mdb_reserved_name(cmd->slotname);
 	ReplicationSlotDrop(cmd->slotname, !cmd->wait);
 }
 
@@ -1271,6 +1313,8 @@ StartLogicalReplication(StartReplicationCmd *cmd)
 {
 	StringInfoData buf;
 	QueryCompletion qc;
+
+	check_mdb_reserved_name(cmd->slotname);
 
 	/* make sure that our requirements are still fulfilled */
 	CheckLogicalDecodingRequirements();
@@ -1827,6 +1871,7 @@ exec_replication_command(const char *cmd_string)
 			break;
 
 		case T_BaseBackupCmd:
+			check_permissions();
 			cmdtag = "BASE_BACKUP";
 			set_ps_display(cmdtag);
 			PreventInTransactionBlock(true, cmdtag);
