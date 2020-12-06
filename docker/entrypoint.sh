b89030378975d6b03a1b6beae67d7008f8b31f19
Andrey Borodin <amborodin@acm.org>
2020-12-06 10:01:19 +0500

    Debian folder
    Add pg_version.sh
    
    remove pg version & changelog generation
    
    remove stip version
    
    Use mirror.yandex.ru instead of archives.ubuntu.com
    
    mdb_replication role patch

---
+++ docker/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+export TZ=Europe/Moskow
+sudo bash -c "echo $TZ > /etc/timezone"
+
+cd /home/build-user
+
+sudo ./docker/tzdata.sh
+git apply debian/yandex-patches/*
+
+cat debian/changelog
+
+sudo mk-build-deps  --build-dep --install --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' debian/control
+
+dpkg-buildpackage -us -uc
+
+cd /home
+rm -fr build-user
+
