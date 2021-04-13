3843908da0641871f2c2cdf93dd8dbcac127a582
reshke <reshke@double.cloud>
2021-04-13 16:26:33 +0300

    Mdb-admin patch and regression tests
    Introduces 3 functions:
    extern bool mdb_admin_allow_bypass_owner_checks(Oid userId,  Oid ownerId);
    extern void check_mdb_admin_is_member_of_role(Oid member, Oid role);
    extern bool mdb_admin_is_member_of_role(Oid member, Oid role);
    
    To check mdb admin belongship and role-to-role ownership transfer
    correctness.
    
    Our mdb_admin ACL model is the following:
    
    * Any roles user or/and roles can be granted with mdb_admin
    * mdb_admin member can tranfser ownershup of relations,
    namespaces and functions to other roles, if target role in neither:
    superuser, pg_read_server_files, pg_write_server_files nor
    pg_execute_server_program.
    
    * Allow mdb_admin to create LEAKPROOF functions
    * mdb admin sets session replication role
    * [MDB-16648 + MDB-17910]: Allow mdb admin to kill specific superuser queries
    
    MDB-27288: allow mdb_admin to kill autovac + tests
    
    mdb-27228: fix expected output

--- src/test/regress/sql/test_setup.sql
+++ src/test/regress/sql/test_setup.sql
@@ -299,3 +299,5 @@ create function fipshash(text)
     returns text
     strict immutable parallel safe leakproof
     return substr(encode(sha256($1::bytea), 'hex'), 1, 32);
+
+CREATE ROLE mdb_admin;
