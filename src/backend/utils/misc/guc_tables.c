2b2456191ee1c5e61f971ebe5d08c9e7b54e9736
reshke <reshkekirill@gmail.com>
2022-02-22 23:26:51 +0300

    provide [mdb -postgresql] restict grant roles in YC[MDB-16990]

--- src/backend/utils/misc/guc_tables.c
+++ src/backend/utils/misc/guc_tables.c
@@ -91,6 +91,10 @@
 #include "utils/ps_status.h"
 #include "utils/xml.h"
 
+/* MDB patch */
+#include "access/yc_checker.h"
+/**/
+
 /* This value is normally passed in from the Makefile */
 #ifndef PG_KRB_SRVTAB
 #define PG_KRB_SRVTAB ""
@@ -492,6 +496,15 @@ static const struct config_enum_entry file_extend_method_options[] = {
 	{"write_zeros", FILE_EXTEND_METHOD_WRITE_ZEROS, false},
 	{NULL, 0, false}
 };
+
+
+static const struct config_enum_entry yc_grant_checker_options[] = {
+	{"off", YC_GRANT_CHECKER_OFF, false},
+	{"warn", YC_GRANT_CHECKER_WARN, false},
+	{"crit", YC_GRANT_CHECKER_CRIT, false},
+	{NULL, 0, false}
+};
+
 
 /*
  * Options for enum values stored in other modules
@@ -4838,6 +4851,16 @@ struct config_enum ConfigureNamesEnum[] =
 		NULL, NULL, NULL
 	},
 
+	{
+		{"ycmdb.yc_grant_checker", PGC_SUSET, CLIENT_CONN_STATEMENT,
+			gettext_noop("Enables YC MDB runtime checker, which check if user is ok to grant roles to other users."),
+			NULL
+		},
+		((int *) &yc_grant_checker_type),
+		YC_GRANT_CHECKER_OFF,
+		yc_grant_checker_options,
+		NULL, NULL, NULL
+	},
 	{
 		{"default_transaction_isolation", PGC_USERSET, CLIENT_CONN_STATEMENT,
 			gettext_noop("Sets the transaction isolation level of each new transaction."),
