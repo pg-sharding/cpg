81169543e97f9b9a391145fc201026a28d7912b2
reshke <reshkekirill@gmail.com>
2022-02-22 23:26:51 +0300

    provide [mdb -postgresql] restict grant roles in YC[MDB-16990]

---
+++ src/include/access/yc_checker.h
@@ -0,0 +1,31 @@
+#
+/*-------------------------------------------------------------------------
+ *
+ * yc_checker.h
+ *
+ *  Header file for YC MDB specific only GUC variables, 
+ *
+ * Portions Copyright (c) 1996-2021, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ * src/include/storage/yc_checker.h
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef PG_YC_CHECKER_H
+#define PG_YC_CHECKER_H
+
+/* Possible values for yc_grant_checker_type */
+typedef enum
+{
+	YC_GRANT_CHECKER_OFF,
+	YC_GRANT_CHECKER_WARN,
+	YC_GRANT_CHECKER_CRIT,
+} YCGrantCheckerType;
+
+/* GUC variables */
+
+YCGrantCheckerType yc_grant_checker_type;
+
+
+#endif /* PG_YC_CHECKER_H */
