7bb0c9151368323fb5ad29ebff339aba452d547e
Kirill Reshke <reshkekirill@gmail.com>
2022-03-11 13:19:51 +0300

    MDB admin patch & tests
    Allow mdb admin to tranfers ownership on non-superuser objects regressioon tests for mdb admin functionality[MDB-16988] (#14)
    
    * mdb admin regression tests
    
    * Allow mdb admin to tranfers ownership on non-superuser objects
    
    * more tests
    
    * mdb admin bypass usage privilege check for schema lookup
    
    Allow mdb_admin to create LEAKPROOF functions
    
    mdb admin sets session replication role
    
    Fix mdb session role config param
    
    [MDB-16648]: Allow mdb admin to kill specific superuser queries
    
    Fix tests for mdb admin role creation
    
    Reimplement mdb-admin, refactor mdb_admin check and usages.
    
    Introduces 2 new function:
    extern bool mdb_admin_allow_bypass_owner_checks(Oid userId,  Oid ownerId);
    extern void check_mdb_admin_is_member_of_role(Oid member, Oid role);
    
    To check mdb admin belongship and role-to-role ownership transfer
    correctness.
    
    Our mdb_admin ACL model is the following:
    
    * Any roles user or/and roles can be granted with mdb_admin
    * mdb_admin memeber can tranfser ownershup of relations,
    namespaces and functions to other roles, if target role in neither:
    superuser, pg_read_server_files, pg_write_server_files nor
    pg_execute_server_program.
    
    MDB-27288: allow mdb_admin to kill autovac + tests
    
    Fix mdb admin patch with stale pgstat snapshot + MDB-28431

--- src/backend/commands/functioncmds.c
+++ src/backend/commands/functioncmds.c
@@ -1160,9 +1160,13 @@ CreateFunction(ParseState *pstate, CreateFunctionStmt *stmt)
 	 * by security barrier views or row-level security policies.
 	 */
 	if (isLeakProof && !superuser())
-		ereport(ERROR,
-				(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
-				 errmsg("only superuser can define a leakproof function")));
+	{
+		Oid role = get_role_oid("mdb_admin", true);
+		if (!is_member_of_role(GetUserId(), role))
+			ereport(ERROR,
+					(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+					errmsg("only superuser or mdb_admin can define a leakproof function")));
+	}
 
 	if (transformDefElem)
 	{
@@ -1444,9 +1448,13 @@ AlterFunction(ParseState *pstate, AlterFunctionStmt *stmt)
 	{
 		procForm->proleakproof = intVal(leakproof_item->arg);
 		if (procForm->proleakproof && !superuser())
-			ereport(ERROR,
-					(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
-					 errmsg("only superuser can define a leakproof function")));
+		{
+			Oid role = get_role_oid("mdb_admin", true);
+			if (!is_member_of_role(GetUserId(), role))
+				ereport(ERROR,
+						(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
+						errmsg("only superuser or mdb_admin can define a leakproof function")));
+		}
 	}
 	if (cost_item)
 	{
