b7d0e1ec91b1989b8846c6c9a4e4f922266f101c
Andrey M. Borodin <x4mmm@172.25.72.45-ekb.dhcp.yndx.net>
2022-09-12 11:47:30 +0500

    Demonstrate and fix lock of all SQL queries by pg_stat_statements

--- contrib/pg_stat_statements/pg_stat_statements.c
+++ contrib/pg_stat_statements/pg_stat_statements.c
@@ -1313,8 +1313,12 @@ pgss_store(const char *query, int64 queryId,
 	key.queryid = queryId;
 	key.toplevel = (nesting_level == 0);
 
-	/* Lookup the hash table entry with shared lock. */
-	LWLockAcquire(&pgss->lock.lock, LW_SHARED);
+	/*
+	 * Lookup the hash table entry with shared lock.
+	 * If exclusive lock is taken - just give up.
+	 */
+	if (!LWLockConditionalAcquire(&pgss->lock.lock, LW_SHARED))
+		return;
 
 	entry = (pgssEntry *) hash_search(pgss_hash, &key, HASH_FIND, NULL);
 
@@ -1339,7 +1343,13 @@ pgss_store(const char *query, int64 queryId,
 			norm_query = generate_normalized_query(jstate, query,
 												   query_location,
 												   &query_len);
-			LWLockAcquire(&pgss->lock.lock, LW_SHARED);
+			/* exclusive lock may be taken while we were doing this */
+			/* XXX: Andrey: I'm not sure we should drop here shared lock at all */
+			if (!LWLockConditionalAcquire(&pgss->lock.lock, LW_SHARED))
+			{
+				pfree(norm_query);
+				return;
+			}
 		}
 
 		/* Append new query text to file with only shared lock held */
@@ -1355,7 +1365,9 @@ pgss_store(const char *query, int64 queryId,
 
 		/* Need exclusive lock to make a new hashtable entry - promote */
 		LWLockRelease(&pgss->lock.lock);
-		LWLockAcquire(&pgss->lock.lock, LW_EXCLUSIVE);
+		/* This renders impossible to enter another concurrent query */
+		if (!LWLockConditionalAcquire(&pgss->lock.lock, LW_EXCLUSIVE))
+			return;
 
 		/*
 		 * A garbage collection may have occurred while we weren't holding the
