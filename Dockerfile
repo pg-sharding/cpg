9011ed356b017b9b30e9df645f71b3db29c1356d
usernamedt <usernamedt@yandex-team.com>
2023-02-13 15:00:31 +0800

    Implement mdb-locales patch
    Add mdb locales patch, restore COPY from/to files, enable regress.
    
    Merge in MDB/postgres-dev from pg15_mdb-locale to MDB_15_STABLE
    
    Squashed commit of the following:
    
    commit 9f8ea4a5f42e0fd6077061bafbb428e88499896f
    Author: reshke kirill <reshke@double.cloud>
    Date:   Wed Feb 22 09:27:53 2023 +0000
    
        Add mdb locales patch, restore COPY from/to files, enable regress.
    
        This commit does several things.
        * Enables back COPY from/to FILE functionality, becuase it is used
        by pg_regress
        * Enables pg_regress tests in deb build itself.
        * Add mdb locales function and checks that package build with
        mdb-locales support
    
    Add configure ac target to define USE_MDBLOCALES properly

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,8 @@ ENV DEBIAN_FRONTEND=noninteractive
 ENV TZ=Europe/Moskow
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-RUN sed -i 's/archive.ubuntu.com/mirror.yandex.ru/g' /etc/apt/sources.list
-
-RUN apt-get update && apt-get install -y --no-install-recommends \
+RUN sed -i 's/archive.ubuntu.com/mirror.yandex.ru/g' /etc/apt/sources.list &&\
+    apt-get update && apt-get install -y --no-install-recommends \
     sudo build-essential \
     gcc lsb-release libssl-dev gnupg openssl \
     gdb git \
@@ -17,6 +16,11 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     libsystemd-dev libxml2-dev libxml2-utils libxslt1-dev \
     pkg-config python3-dev systemtap-sdt-dev tcl-dev uuid-dev xsltproc zlib1g-dev \
     bison dh-exec docbook-xml docbook-xsl
+
+RUN echo 'deb http://dist.yandex.ru/mdb-bionic-secure stable/all/' | tee -a /etc/apt/sources.list &&\
+    echo 'deb http://dist.yandex.ru/mdb-bionic-secure stable/amd64/' | tee -a /etc/apt/sources.list &&\
+    apt-get update -o Acquire::AllowInsecureRepositories=true &&\
+    apt-get install -y --no-install-recommends --allow-unauthenticated libmdblocales1 libmdblocales-dev
 
 RUN groupadd -g 999 build-user && \
     useradd -r -u 999 -g build-user build-user
--- src/backend/utils/adt/pg_locale.c
+++ src/backend/utils/adt/pg_locale.c
@@ -66,6 +66,7 @@
 #include "utils/pg_locale.h"
 #include "utils/relcache.h"
 #include "utils/syscache.h"
+#include <mdblocales.h>
 
 #ifdef USE_ICU
 #include <unicode/ucnv.h>
@@ -147,7 +148,7 @@ pg_perm_setlocale(int category, const char *locale)
 	const char *envvar;
 
 #ifndef WIN32
-	result = setlocale(category, locale);
+	result = mdb_setlocale(category, locale);
 #else
 
 	/*
@@ -252,7 +253,7 @@ check_locale(int category, const char *locale, char **canonname)
 	if (canonname)
 		*canonname = NULL;		/* in case of failure */
 
-	save = setlocale(category, NULL);
+	save = mdb_setlocale(category, NULL);
 	if (!save)
 		return false;			/* won't happen, we hope */
 
@@ -260,7 +261,7 @@ check_locale(int category, const char *locale, char **canonname)
 	save = pstrdup(save);
 
 	/* set the locale with setlocale, to see if it accepts it. */
-	res = setlocale(category, locale);
+	res = mdb_setlocale(category, locale);
 
 	/* save canonical name if requested. */
 	if (res && canonname)
@@ -501,12 +502,12 @@ PGLC_localeconv(void)
 	memset(&worklconv, 0, sizeof(worklconv));
 
 	/* Save prevailing values of monetary and numeric locales */
-	save_lc_monetary = setlocale(LC_MONETARY, NULL);
+	save_lc_monetary = mdb_setlocale(LC_MONETARY, NULL);
 	if (!save_lc_monetary)
 		elog(ERROR, "setlocale(NULL) failed");
 	save_lc_monetary = pstrdup(save_lc_monetary);
 
-	save_lc_numeric = setlocale(LC_NUMERIC, NULL);
+	save_lc_numeric = mdb_setlocale(LC_NUMERIC, NULL);
 	if (!save_lc_numeric)
 		elog(ERROR, "setlocale(NULL) failed");
 	save_lc_numeric = pstrdup(save_lc_numeric);
@@ -773,7 +774,7 @@ cache_locale_time(void)
 	 */
 
 	/* Save prevailing value of time locale */
-	save_lc_time = setlocale(LC_TIME, NULL);
+	save_lc_time = mdb_setlocale(LC_TIME, NULL);
 	if (!save_lc_time)
 		elog(ERROR, "setlocale(NULL) failed");
 	save_lc_time = pstrdup(save_lc_time);
@@ -1225,7 +1226,7 @@ check_strxfrm_bug(void)
 		ereport(ERROR,
 				(errcode(ERRCODE_SYSTEM_ERROR),
 				 errmsg_internal("strxfrm(), in locale \"%s\", writes past the specified array length",
-								 setlocale(LC_COLLATE, NULL)),
+								 mdb_setlocale(LC_COLLATE, NULL)),
 				 errhint("Apply system library package updates.")));
 }
 
@@ -1341,7 +1342,7 @@ lc_collate_is_c(Oid collation)
 
 		if (result >= 0)
 			return (bool) result;
-		localeptr = setlocale(LC_COLLATE, NULL);
+		localeptr = mdb_setlocale(LC_COLLATE, NULL);
 		if (!localeptr)
 			elog(ERROR, "invalid LC_COLLATE setting");
 
@@ -1391,7 +1392,7 @@ lc_ctype_is_c(Oid collation)
 
 		if (result >= 0)
 			return (bool) result;
-		localeptr = setlocale(LC_CTYPE, NULL);
+		localeptr = mdb_setlocale(LC_CTYPE, NULL);
 		if (!localeptr)
 			elog(ERROR, "invalid LC_CTYPE setting");
 
@@ -1516,7 +1517,7 @@ pg_newlocale_from_collation(Oid collid)
 				/* Normal case where they're the same */
 				errno = 0;
 #ifndef WIN32
-				loc = newlocale(LC_COLLATE_MASK | LC_CTYPE_MASK, collcollate,
+				loc = mdb_newlocale(LC_COLLATE_MASK | LC_CTYPE_MASK, collcollate,
 								NULL);
 #else
 				loc = _create_locale(LC_ALL, collcollate);
@@ -1531,11 +1532,11 @@ pg_newlocale_from_collation(Oid collid)
 				locale_t	loc1;
 
 				errno = 0;
-				loc1 = newlocale(LC_COLLATE_MASK, collcollate, NULL);
+				loc1 = mdb_newlocale(LC_COLLATE_MASK, collcollate, NULL);
 				if (!loc1)
 					report_newlocale_failure(collcollate);
 				errno = 0;
-				loc = newlocale(LC_CTYPE_MASK, collctype, loc1);
+				loc = mdb_newlocale(LC_CTYPE_MASK, collctype, loc1);
 				if (!loc)
 					report_newlocale_failure(collctype);
 #else
