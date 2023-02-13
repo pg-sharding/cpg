6f02acfd6693d361c682a62aaca8f3c4c64d9ef9
usernamedt <usernamedt@yandex-team.com>
2023-02-13 15:00:31 +0800

    Implement mdb-locales patch
    Do not use mdb_locales and mdb_newlocale when configured without them.
    
    Added ifdef codepath build without mdb-locales feature
    
    Add mdb locales patch, restore COPY from/to files, enable regress.
    
    Squashed commit of the following:
    
    commit 9f8ea4a5f42e0fd6077061bafbb428e88499896f
    Author: reshke kirill <reshke@double.cloud>
    Date:   Wed Feb 22 09:27:53 2023 +0000
    
        Add mdb locales patch, restore COPY from/to files, enable regress.
    
        This commit does several things.
        * Enables back COPY from/to FILE functionality, becuase it is used
        by pg_regress
        * Enables pg_regress tests in deb build itself.
        * Add mdb locales function and checks that package build with
        mdb-locales support
    
    Add configure ac target to define USE_MDBLOCALES properly
    
    Refactor optional setlocale, fix minor issues

--- src/backend/utils/mb/mbutils.c
+++ src/backend/utils/mb/mbutils.c
@@ -43,6 +43,7 @@
 #include "utils/relcache.h"
 #include "utils/syscache.h"
 #include "varatt.h"
+#include "common/mdb_locale.h"
 
 /*
  * We maintain a simple linked list caching the fmgr lookup info for the
@@ -1363,7 +1364,7 @@ pg_bind_textdomain_codeset(const char *domainname)
 	int			new_msgenc;
 
 #ifndef WIN32
-	const char *ctype = setlocale(LC_CTYPE, NULL);
+	const char *ctype = SETLOCALE(LC_CTYPE, NULL);
 
 	if (pg_strcasecmp(ctype, "C") == 0 || pg_strcasecmp(ctype, "POSIX") == 0)
 #endif
