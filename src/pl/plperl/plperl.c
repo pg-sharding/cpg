f279dcb245a4e08ddc50f68c5faa492f89d5ce3d
usernamedt <usernamedt@yandex-team.com>
2023-02-13 15:00:31 +0800

    Implement mdb-locales patch
    Do not use mdb_locales and mdb_newlocale when configured without them.
    
    Added ifdef codepath build without mdb-locales feature
    
    Add mdb locales patch, restore COPY from/to files, enable regress.
    
    Squashed commit of the following:
    
    commit 9f8ea4a5f42e0fd6077061bafbb428e88499896f
    Author: reshke kirill <reshke@double.cloud>
    Date:   Wed Feb 22 09:27:53 2023 +0000
    
        Add mdb locales patch, restore COPY from/to files, enable regress.
    
        This commit does several things.
        * Enables back COPY from/to FILE functionality, becuase it is used
        by pg_regress
        * Enables pg_regress tests in deb build itself.
        * Add mdb locales function and checks that package build with
        mdb-locales support
    
    Add configure ac target to define USE_MDBLOCALES properly
    
    Refactor optional setlocale, fix minor issues

--- src/pl/plperl/plperl.c
+++ src/pl/plperl/plperl.c
@@ -36,6 +36,7 @@
 #include "utils/rel.h"
 #include "utils/syscache.h"
 #include "utils/typcache.h"
+#include "common/mdb_locale.h"
 
 /* define our text domain for translations */
 #undef TEXTDOMAIN
@@ -742,15 +743,15 @@ plperl_init_interp(void)
 			   *save_numeric,
 			   *save_time;
 
-	loc = setlocale(LC_COLLATE, NULL);
+	loc = SETLOCALE(LC_COLLATE, NULL);
 	save_collate = loc ? pstrdup(loc) : NULL;
-	loc = setlocale(LC_CTYPE, NULL);
+	loc = SETLOCALE(LC_CTYPE, NULL);
 	save_ctype = loc ? pstrdup(loc) : NULL;
-	loc = setlocale(LC_MONETARY, NULL);
+	loc = SETLOCALE(LC_MONETARY, NULL);
 	save_monetary = loc ? pstrdup(loc) : NULL;
-	loc = setlocale(LC_NUMERIC, NULL);
+	loc = SETLOCALE(LC_NUMERIC, NULL);
 	save_numeric = loc ? pstrdup(loc) : NULL;
-	loc = setlocale(LC_TIME, NULL);
+	loc = SETLOCALE(LC_TIME, NULL);
 	save_time = loc ? pstrdup(loc) : NULL;
 
 #define PLPERL_RESTORE_LOCALE(name, saved) \
@@ -4182,7 +4183,7 @@ static char *
 setlocale_perl(int category, char *locale)
 {
 	dTHX;
-	char	   *RETVAL = setlocale(category, locale);
+	char	   *RETVAL = SETLOCALE(category, locale);
 
 	if (RETVAL)
 	{
@@ -4197,7 +4198,7 @@ setlocale_perl(int category, char *locale)
 
 #ifdef LC_ALL
 			if (category == LC_ALL)
-				newctype = setlocale(LC_CTYPE, NULL);
+				newctype = SETLOCALE(LC_CTYPE, NULL);
 			else
 #endif
 				newctype = RETVAL;
@@ -4215,7 +4216,7 @@ setlocale_perl(int category, char *locale)
 
 #ifdef LC_ALL
 			if (category == LC_ALL)
-				newcoll = setlocale(LC_COLLATE, NULL);
+				newcoll = SETLOCALE(LC_COLLATE, NULL);
 			else
 #endif
 				newcoll = RETVAL;
@@ -4234,7 +4235,7 @@ setlocale_perl(int category, char *locale)
 
 #ifdef LC_ALL
 			if (category == LC_ALL)
-				newnum = setlocale(LC_NUMERIC, NULL);
+				newnum = SETLOCALE(LC_NUMERIC, NULL);
 			else
 #endif
 				newnum = RETVAL;
