d8a176e98161b3d5f3c90324210e02966d0a8f22
usernamedt <usernamedt@yandex-team.com>
2023-02-13 15:00:31 +0800

    Implement mdb-locales patch
    Add mdb locales patch, restore COPY from/to files, enable regress.
    
    Merge in MDB/postgres-dev from pg15_mdb-locale to MDB_15_STABLE
    
    Squashed commit of the following:
    
    commit 9f8ea4a5f42e0fd6077061bafbb428e88499896f
    Author: reshke kirill <reshke@double.cloud>
    Date:   Wed Feb 22 09:27:53 2023 +0000
    
        Add mdb locales patch, restore COPY from/to files, enable regress.
    
        This commit does several things.
        * Enables back COPY from/to FILE functionality, becuase it is used
        by pg_regress
        * Enables pg_regress tests in deb build itself.
        * Add mdb locales function and checks that package build with
        mdb-locales support
    
    Add configure ac target to define USE_MDBLOCALES properly

--- src/common/exec.c
+++ src/common/exec.c
@@ -24,6 +24,7 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <mdblocales.h>
 
 #ifdef EXEC_BACKEND
 #if defined(HAVE_SYS_PERSONALITY_H)
@@ -451,7 +452,7 @@ set_pglocale_pgservice(const char *argv0, const char *app)
 	/* don't set LC_ALL in the backend */
 	if (strcmp(app, PG_TEXTDOMAIN("postgres")) != 0)
 	{
-		setlocale(LC_ALL, "");
+		mdb_setlocale(LC_ALL, "");
 
 		/*
 		 * One could make a case for reproducing here PostmasterMain()'s test
