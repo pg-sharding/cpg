6b5b414beb0ff49d058bf57c87d61cdab0fc4e15
usernamedt <usernamedt@yandex-team.com>
2023-02-13 15:00:31 +0800

    Implement mdb-locales patch
    Do not use mdb_locales and mdb_newlocale when configured without them.
    
    Added ifdef codepath build without mdb-locales feature
    
    Add mdb locales patch, restore COPY from/to files, enable regress.
    
    Squashed commit of the following:
    
    commit 9f8ea4a5f42e0fd6077061bafbb428e88499896f
    Author: reshke kirill <reshke@double.cloud>
    Date:   Wed Feb 22 09:27:53 2023 +0000
    
        Add mdb locales patch, restore COPY from/to files, enable regress.
    
        This commit does several things.
        * Enables back COPY from/to FILE functionality, becuase it is used
        by pg_regress
        * Enables pg_regress tests in deb build itself.
        * Add mdb locales function and checks that package build with
        mdb-locales support
    
    Add configure ac target to define USE_MDBLOCALES properly
    
    Refactor optional setlocale, fix minor issues
    
    Add missing SETLOCALE and NEWLOCALE calls.
    
    They seem to be missing after 37851a8b83d3 and 4f5cef2607c1
    
    Co-authored-by: reshke reshke@double.cloud

--- src/interfaces/ecpg/ecpglib/execute.c
+++ src/interfaces/ecpg/ecpglib/execute.c
@@ -31,6 +31,7 @@
 #include "sqlca.h"
 #include "sqlda-compat.h"
 #include "sqlda-native.h"
+#include "common/mdb_locale.h"
 
 /*
  *	This function returns a newly malloced string that has ' and \
@@ -2001,13 +2002,13 @@ ecpg_do_prologue(int lineno, const int compat, const int force_indicator,
 		return false;
 	}
 #endif
-	stmt->oldlocale = ecpg_strdup(setlocale(LC_NUMERIC, NULL), lineno);
+	stmt->oldlocale = ecpg_strdup(SETLOCALE(LC_NUMERIC, NULL), lineno);
 	if (stmt->oldlocale == NULL)
 	{
 		ecpg_do_epilogue(stmt);
 		return false;
 	}
-	setlocale(LC_NUMERIC, "C");
+    SETLOCALE(LC_NUMERIC, "C");
 #endif
 
 	/*
@@ -2222,7 +2223,7 @@ ecpg_do_epilogue(struct statement *stmt)
 #else
 	if (stmt->oldlocale)
 	{
-		setlocale(LC_NUMERIC, stmt->oldlocale);
+		SETLOCALE(LC_NUMERIC, stmt->oldlocale);
 #ifdef WIN32
 		_configthreadlocale(stmt->oldthreadlocale);
 #endif
