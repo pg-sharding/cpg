36651c0d842325ebe70117149c68c82f5765a771
reshke kirill <reshke@double.cloud>
2023-02-21 19:34:14 +0000

    Role mdb_superuser: feature and regress testsing
    This commit introduces new mdb internal role mdb_superuser.
    
    Role is capaple of:
    
    GRANT/REVOKE any set of priviledges to/from any object in database.
    Has power of pg_database_owner in any database, including:
    DROP any object in database (except system catalog and stuff)
    
    Role is NOT capaple of:
    
    Create database, role, extension or alter other roles with such
    priviledges.
    
    Transfer ownership to /pass has_priv of roles:
    
    PG_READ_ALL_DATA
    PG_WRITE_ALL_DATA
    PG_EXECUTE_SERVER_PROGRAM
    PG_READ_SERVER_FILES
    PG_WRITE_SERVER_FILES
    
    Allow mdb_superuser to have power of pg_database_owner
    
    Allow mdb_superuser to alter objects and grant ACl to
    objects, owned by pg_database_owner. Also, when acl check,
    allow mdb_superuser use pg_database_owner role power to pass check
    
    MDB-32132: fix grantor selection for mdb_superuser (#6)
    
    Do not use schema public in mdb_superuser regression tests (#7)

--- src/backend/utils/adt/acl.c
+++ src/backend/utils/adt/acl.c
@@ -5320,7 +5320,7 @@ roles_is_member_of(Oid roleid, enum RoleRecurseType type,
 * This is basically original postgresql privs-check function
 */
 
-static bool
+bool
 has_privs_of_role_strict(Oid member, Oid role)
 {
 	/* Fast path for simple case */
@@ -5342,7 +5342,7 @@ has_privs_of_role_strict(Oid member, Oid role)
 
 /*
 * Check that role is either one of "dangerous" system role
-* or has "strict" (not through mdb_admin) 
+* or has "strict" (not through mdb_admin or mdb_superuser) 
 * privs of this role
 */
 
@@ -5370,6 +5370,8 @@ has_privs_of_unwanted_system_role(Oid role) {
 bool
 has_privs_of_role(Oid member, Oid role)
 {
+	Oid mdb_superuser_roleoid;
+
 	/* Fast path for simple case */
 	if (member == role)
 		return true;
@@ -5377,6 +5379,23 @@ has_privs_of_role(Oid member, Oid role)
 	/* Superusers have every privilege, so are part of every role */
 	if (superuser_arg(member))
 		return true;
+
+	mdb_superuser_roleoid = get_role_oid("mdb_superuser", true /*if nodoby created mdb_superuser role in this database*/);
+
+	if (is_member_of_role(member, mdb_superuser_roleoid)) {
+		/* if target role is superuser, disallow */
+		if (!superuser_arg(role)) {
+			/* we want mdb_roles_admin to bypass
+			* has_priv_of_roles test
+			* if target role is neither superuser nor
+			* some dangerous system role
+			*/
+			if (!has_privs_of_unwanted_system_role(role)) {
+				return true;
+			}
+		}
+	}
+	
 
 	/*
 	 * Find all the roles that member has the privileges of, including
@@ -5386,6 +5405,8 @@ has_privs_of_role(Oid member, Oid role)
 											  InvalidOid, NULL),
 						   role);
 }
+
+// -- mdb_superuser patch
 
 // -- non-upstream patch begin
 /*
@@ -5679,6 +5700,7 @@ select_best_grantor(const RoleSpec *grantedBy, AclMode privileges,
 	List	   *roles_list;
 	int			nrights;
 	ListCell   *l;
+	Oid			mdb_superuser_roleoid;
 
 	/*
 	 * If we have GRANTED BY, resolve it and verify current user is allowed to
@@ -5710,6 +5732,16 @@ select_best_grantor(const RoleSpec *grantedBy, AclMode privileges,
 	{
 		*grantorId = ownerId;
 		*grantOptions = needed_goptions;
+		return;
+	}
+
+	mdb_superuser_roleoid = get_role_oid("mdb_superuser", true /*if nodoby created mdb_superuser role in this database*/);
+
+	if (is_member_of_role(GetUserId(), mdb_superuser_roleoid)
+	&& has_privs_of_role(GetUserId(), ownerId)) {
+		*grantorId = ownerId;
+		AclMode mdb_superuser_allowed_privs = needed_goptions;
+		*grantOptions = mdb_superuser_allowed_privs;
 		return;
 	}
 
--- src/include/utils/acl.h
+++ src/include/utils/acl.h
@@ -213,6 +213,7 @@ extern int	aclmembers(const Acl *acl, Oid **roleids);
 extern bool has_privs_of_role(Oid member, Oid role);
 extern bool member_can_set_role(Oid member, Oid role);
 extern void check_can_set_role(Oid member, Oid role);
+extern bool has_privs_of_role_strict(Oid member, Oid role);
 extern bool is_member_of_role(Oid member, Oid role);
 extern bool is_member_of_role_nosuper(Oid member, Oid role);
 extern bool is_admin_of_role(Oid member, Oid role);
