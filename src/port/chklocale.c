e3261539eea92e1e30429db750834f89a7dc5550
reshke kirill <reshke@double.cloud>
2023-02-21 19:17:07 +0000

    Do not use mdb_locales and mdb_newlocale when configured without them.
    Added ifdef codepath build without mdb-locales feature
    
    Refactor optional setlocale, fix minor issues
    
    Update dependencies: bump libmdblocales, add mdb-locales

--- src/port/chklocale.c
+++ src/port/chklocale.c
@@ -18,7 +18,8 @@
 #else
 #include "postgres_fe.h"
 #endif
-#include <mdblocales.h>
+#include "common/mdb_locale.h"
+
 
 #ifdef HAVE_LANGINFO_H
 #include <langinfo.h>
@@ -344,7 +345,7 @@ pg_get_encoding_from_locale(const char *ctype, bool write_message)
 			pg_strcasecmp(ctype, "POSIX") == 0)
 			return PG_SQL_ASCII;
 
-		save = mdb_setlocale(LC_CTYPE, NULL);
+		save = SETLOCALE(LC_CTYPE, NULL);
 		if (!save)
 			return -1;			/* setlocale() broken? */
 		/* must copy result, or it might change after setlocale */
@@ -352,7 +353,7 @@ pg_get_encoding_from_locale(const char *ctype, bool write_message)
 		if (!save)
 			return -1;			/* out of memory; unlikely */
 
-		name = mdb_setlocale(LC_CTYPE, ctype);
+		name = SETLOCALE(LC_CTYPE, ctype);
 		if (!name)
 		{
 			free(save);
@@ -367,13 +368,13 @@ pg_get_encoding_from_locale(const char *ctype, bool write_message)
 		sys = win32_langinfo(name);
 #endif
 
-		mdb_setlocale(LC_CTYPE, save);
+		SETLOCALE(LC_CTYPE, save);
 		free(save);
 	}
 	else
 	{
 		/* much easier... */
-		ctype = mdb_setlocale(LC_CTYPE, NULL);
+		ctype = SETLOCALE(LC_CTYPE, NULL);
 		if (!ctype)
 			return -1;			/* setlocale() broken? */
 
