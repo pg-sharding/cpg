2ea722169ce9cb77834cca9c69975471aa7ee318
reshke kirill <reshke@double.cloud>
2023-02-21 19:34:14 +0000

    Role mdb_superuser: feature and regress testsing
    This commit introduces new mdb internal role mdb_superuser.
    
    Role is capaple of:
    
    GRANT/REVOKE any set of priviledges to/from any object in database.
    Has power of pg_database_owner in any database, including:
    DROP any object in database (except system catalog and stuff)
    
    Role is NOT capaple of:
    
    Create database, role, extension or alter other roles with such
    priviledges.
    
    Transfer ownership to /pass has_priv of roles:
    
    PG_READ_ALL_DATA
    PG_WRITE_ALL_DATA
    PG_EXECUTE_SERVER_PROGRAM
    PG_READ_SERVER_FILES
    PG_WRITE_SERVER_FILES
    
    Fix configure.ac USE_MDBLOCALES option handling
    
    Apply autoreconf stuff
    
    Set missing ok parameter ito true while acquiring mdb_superuser oid
    
    In regress tests, nobody creates mdb_superuser role, so missing ok is
    fine
    
    Fix spelling
    
    Applied suggestion
    
    Allow mdb_superuser to have power of pg_database_owner
    
    Allow mdb_superuser to alter objects and grant ACl to
    objects, owned by pg_database_owner. Also, when acl check,
    allow mdb_superuser use pg_database_owner role power to pass check
    
    regression test fixes
    
    MDB-32132: fix grantor selection for mdb_superuser (#6)

--- src/backend/utils/adt/acl.c
+++ src/backend/utils/adt/acl.c
@@ -5310,7 +5310,7 @@ has_privs_of_role_strict(Oid member, Oid role)
 
 /*
 * Check that role is either one of "dangerous" system role
-* or has "strict" (not through mdb_admin) 
+* or has "strict" (not through mdb_admin or mdb_superuser) 
 * privs of this role
 */
 
@@ -5338,6 +5338,8 @@ has_privs_of_unwanted_system_role(Oid role) {
 bool
 has_privs_of_role(Oid member, Oid role)
 {
+	Oid mdb_superuser_roleoid;
+
 	/* Fast path for simple case */
 	if (member == role)
 		return true;
@@ -5345,6 +5347,23 @@ has_privs_of_role(Oid member, Oid role)
 	/* Superusers have every privilege, so are part of every role */
 	if (superuser_arg(member))
 		return true;
+
+	mdb_superuser_roleoid = get_role_oid("mdb_superuser", true /*if nodoby created mdb_superuser role in this database*/);
+
+	if (is_member_of_role(member, mdb_superuser_roleoid)) {
+		/* if target role is superuser, disallow */
+		if (!superuser_arg(role)) {
+			/* we want mdb_roles_admin to bypass
+			* has_priv_of_roles test
+			* if target role is neither superuser nor
+			* some dangerous system role
+			*/
+			if (!has_privs_of_unwanted_system_role(role)) {
+				return true;
+			}
+		}
+	}
+	
 
 	/*
 	 * Find all the roles that member has the privileges of, including
@@ -5354,6 +5373,8 @@ has_privs_of_role(Oid member, Oid role)
 											  InvalidOid, NULL),
 						   role);
 }
+
+// -- mdb_superuser patch
 
 // -- non-upstream patch begin
 /*
@@ -5644,6 +5665,7 @@ select_best_grantor(Oid roleId, AclMode privileges,
 	List	   *roles_list;
 	int			nrights;
 	ListCell   *l;
+	Oid			mdb_superuser_roleoid;
 
 	/*
 	 * The object owner is always treated as having all grant options, so if
@@ -5655,6 +5677,16 @@ select_best_grantor(Oid roleId, AclMode privileges,
 	{
 		*grantorId = ownerId;
 		*grantOptions = needed_goptions;
+		return;
+	}
+
+	mdb_superuser_roleoid = get_role_oid("mdb_superuser", true /*if nodoby created mdb_superuser role in this database*/);
+
+	if (is_member_of_role(GetUserId(), mdb_superuser_roleoid)
+	&& has_privs_of_role(GetUserId(), ownerId)) {
+		*grantorId = ownerId;
+		AclMode mdb_superuser_allowed_privs = needed_goptions;
+		*grantOptions = mdb_superuser_allowed_privs;
 		return;
 	}
 
