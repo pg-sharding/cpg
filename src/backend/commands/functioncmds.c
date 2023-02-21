5a02a114b4774834e7ca272e550a5b28cb2a665e
reshke kirill <reshke@double.cloud>
2023-02-21 19:34:14 +0000

    Role mdb_superuser: feature and regress testsing
    This commit introduces new mdb internal role mdb_superuser.
    
    Role is capaple of:
    
    GRANT/REVOKE any set of priviledges to/from any object in database.
    Has power of pg_database_owner in any database, including:
    DROP any object in database (except system catalog and stuff)
    
    Role is NOT capaple of:
    
    Create database, role, extension or alter other roles with such
    priviledges.
    
    Transfer ownership to /pass has_priv of roles:
    
    PG_READ_ALL_DATA
    PG_WRITE_ALL_DATA
    PG_EXECUTE_SERVER_PROGRAM
    PG_READ_SERVER_FILES
    PG_WRITE_SERVER_FILES
    PG_DATABASE_OWNER
    
    Fix configure.ac USE_MDBLOCALES option handling
    
    Apply autoreconf stuff
    
    Set missing ok parameter ito true while acquiring mdb_superuser oid
    
    In regress tests, nobody creates mdb_superuser role, so missing ok is
    fine
    
    Allow mdb_superuser to have power of pg_database_owner
    
    Allow mdb_superuser to alter objects and grant ACl to
    objects, owner by pg_database_owner. Also, when acl check,
    allow mdb_supersuer use pg_database_owner role power to pass check

--- src/backend/commands/functioncmds.c
+++ src/backend/commands/functioncmds.c
@@ -1161,7 +1161,7 @@ CreateFunction(ParseState *pstate, CreateFunctionStmt *stmt)
 	 */
 	if (isLeakProof && !superuser())
 	{
-		Oid role = get_role_oid("mdb_admin", true);
+		Oid role = get_role_oid("mdb_admin", true /*if nodoby created mdb_admin role in this database*/);
 		if (!is_member_of_role(GetUserId(), role))
 			ereport(ERROR,
 					(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
@@ -1449,7 +1449,7 @@ AlterFunction(ParseState *pstate, AlterFunctionStmt *stmt)
 		procForm->proleakproof = intVal(leakproof_item->arg);
 		if (procForm->proleakproof && !superuser())
 		{
-			Oid role = get_role_oid("mdb_admin", true);
+			Oid role = get_role_oid("mdb_admin", true /*if nodoby created mdb_admin role in this database*/);
 			if (!is_member_of_role(GetUserId(), role))
 				ereport(ERROR,
 						(errcode(ERRCODE_INSUFFICIENT_PRIVILEGE),
