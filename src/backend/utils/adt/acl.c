8fb8f58b972b85cd799b3438371dc0e16fad9892
reshke kirill <reshke@double.cloud>
2023-05-11 08:36:59 +0000

    MDB-23247: startup param for auth passthrough under unpriviledged user
    Accept new startup param _pq_.service_auth_role for service auth under
    any user, which is niether superuser nor have some dangerous system
    role priv
    
    Skip caching routines for pre-startup logic (SCRAM -service auth role)
    
    Add tap-test for mdb service role auth 👍👌😉
    
    MDB-23247: debug ouput for testing purposes lowered to DEBUG5 elog level
    
    Fix regress
    
    Fix tests after rebase
    
    contrib tests 💅️️💅️️💅️️ now works

--- src/backend/utils/adt/acl.c
+++ src/backend/utils/adt/acl.c
@@ -117,8 +117,6 @@ static AclResult pg_role_aclcheck(Oid role_oid, Oid roleid, AclMode mode);
 
 static void RoleMembershipCacheCallback(Datum arg, int cacheid, uint32 hashvalue);
 
-static bool has_privs_of_unwanted_system_role(Oid role);
-
 
 /*
  * Test whether an identifier char can be left unquoted in ACLs.
@@ -4851,7 +4849,7 @@ has_rolinherit(Oid roleid)
  */
 static List *
 roles_is_member_of(Oid roleid, enum RoleRecurseType type,
-				   Oid admin_of, bool *is_admin)
+				   Oid admin_of, bool *is_admin, bool no_cache)
 {
 	Oid			dba;
 	List	   *roles_list;
@@ -4865,7 +4863,6 @@ roles_is_member_of(Oid roleid, enum RoleRecurseType type,
 	if (cached_role[type] == roleid && !OidIsValid(admin_of) &&
 		OidIsValid(cached_role[type]))
 		return cached_roles[type];
-
 	/*
 	 * Role expansion happens in a non-database backend when guc.c checks
 	 * ROLE_PG_READ_ALL_SETTINGS for a physical walsender SHOW command.  In
@@ -4952,7 +4949,10 @@ roles_is_member_of(Oid roleid, enum RoleRecurseType type,
 	cached_role[type] = InvalidOid; /* just paranoia */
 	list_free(cached_roles[type]);
 	cached_roles[type] = new_cached_roles;
-	cached_role[type] = roleid;
+
+	if (!no_cache) {
+		cached_role[type] = roleid;
+	}
 
 	/* And now we can return the answer */
 	return cached_roles[type];
@@ -4989,9 +4989,31 @@ has_privs_of_role_strict(Oid member, Oid role)
 	 * multi-level recursion, then see if target role is any one of them.
 	 */
 	return list_member_oid(roles_is_member_of(member, ROLERECURSE_PRIVS,
-											  InvalidOid, NULL),
+											  InvalidOid, NULL, false),
 						   role);
 }
+
+
+static bool
+has_privs_of_role_strict_no_cache(Oid member, Oid role)
+{
+	/* Fast path for simple case */
+	if (member == role)
+		return true;
+
+	/* Superusers have every privilege, so are part of every role */
+	if (superuser_arg(member))
+		return true;
+	
+	/*
+	 * Find all the roles that member has the privileges of, including
+	 * multi-level recursion, then see if target role is any one of them.
+	 */
+	return list_member_oid(roles_is_member_of(member, ROLERECURSE_PRIVS,
+											  InvalidOid, NULL, true),
+						   role);
+}
+
 
 /*
 * Check that role is either one of "dangerous" system role
@@ -4999,7 +5021,7 @@ has_privs_of_role_strict(Oid member, Oid role)
 * privs of this role
 */
 
-static bool
+bool
 has_privs_of_unwanted_system_role(Oid role) {
 	if (has_privs_of_role_strict(role, ROLE_PG_READ_SERVER_FILES)) {
 		return true;
@@ -5014,6 +5036,26 @@ has_privs_of_unwanted_system_role(Oid role) {
 		return true;
 	}
 	if (has_privs_of_role_strict(role, ROLE_PG_WRITE_ALL_DATA)) {
+		return true;
+	}
+
+	return false;
+}
+
+bool has_privs_of_unwanted_system_role_prestartup(Oid role) {
+	if (has_privs_of_role_strict_no_cache(role, ROLE_PG_READ_SERVER_FILES)) {
+		return true;
+	}
+	if (has_privs_of_role_strict_no_cache(role, ROLE_PG_WRITE_SERVER_FILES)) {
+		return true;
+	}
+	if (has_privs_of_role_strict_no_cache(role, ROLE_PG_EXECUTE_SERVER_PROGRAM)) {
+		return true;
+	}
+	if (has_privs_of_role_strict_no_cache(role, ROLE_PG_READ_ALL_DATA)) {
+		return true;
+	}
+	if (has_privs_of_role_strict_no_cache(role, ROLE_PG_WRITE_ALL_DATA)) {
 		return true;
 	}
 
@@ -5055,7 +5097,7 @@ has_privs_of_role(Oid member, Oid role)
 	 * multi-level recursion, then see if target role is any one of them.
 	 */
 	return list_member_oid(roles_is_member_of(member, ROLERECURSE_PRIVS,
-											  InvalidOid, NULL),
+											  InvalidOid, NULL, false),
 						   role);
 }
 
@@ -5126,7 +5168,7 @@ is_member_of_role(Oid member, Oid role)
 	 * recursion, then see if target role is any one of them.
 	 */
 	return list_member_oid(roles_is_member_of(member, ROLERECURSE_MEMBERS,
-											  InvalidOid, NULL),
+											  InvalidOid, NULL, false),
 						   role);
 }
 
@@ -5211,7 +5253,7 @@ is_member_of_role_nosuper(Oid member, Oid role)
 	 * recursion, then see if target role is any one of them.
 	 */
 	return list_member_oid(roles_is_member_of(member, ROLERECURSE_MEMBERS,
-											  InvalidOid, NULL),
+											  InvalidOid, NULL, false),
 						   role);
 }
 
@@ -5233,7 +5275,7 @@ is_admin_of_role(Oid member, Oid role)
 	if (member == role)
 		return false;
 
-	(void) roles_is_member_of(member, ROLERECURSE_MEMBERS, role, &result);
+	(void) roles_is_member_of(member, ROLERECURSE_MEMBERS, role, &result, false);
 	return result;
 }
 
@@ -5320,7 +5362,7 @@ select_best_grantor(Oid roleId, AclMode privileges,
 	 * doesn't query any role memberships.
 	 */
 	roles_list = roles_is_member_of(roleId, ROLERECURSE_PRIVS,
-									InvalidOid, NULL);
+									InvalidOid, NULL, false);
 	/* initialize candidate result as default */
 	*grantorId = roleId;
 	*grantOptions = ACL_NO_RIGHTS;
