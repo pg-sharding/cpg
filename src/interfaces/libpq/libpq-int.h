f3c0b1d72d9a60540711ef37676181ad7e4a4d16
reshke kirill <reshke@double.cloud>
2023-05-11 08:36:59 +0000

    MDB-23247: startup param for auth passthrough under unpriviledged user
    Accept new startup param _pq_.service_auth_role for service auth under
    any user, which is niether superuser nor have some dangerous system
    role priv
    
    Add tap-test for mdb service role auth 👍👌😉
    
    Fix tests after rebase
    
    contrib tests 💅️️💅️️💅️️ now works
    
    MDB-23247: debug ouput for testing purposes lowered to DEBUG5 elog level
    
    Skip caching routines for pre-startup logic (SCRAM -service auth role)

--- src/interfaces/libpq/libpq-int.h
+++ src/interfaces/libpq/libpq-int.h
@@ -432,6 +432,7 @@ struct pg_conn
 	bool		cancelRequest;	/* true if this connection is used to send a
 								 * cancel request, instead of being a normal
 								 * connection that's used for queries */
+	char       *service_auth_role; /* MDB-23247: option for service log-in */
 
 	/* OAuth v2 */
 	char	   *oauth_issuer;	/* token issuer/URL */
