5305880e81bd7889acec233f60ec5b0bb27cf56e
reshke <reshke@double.cloud>
2023-07-31 09:09:37 +0000

    Git apply debian patches[16]

--- src/include/port/pg_iovec.h
+++ src/include/port/pg_iovec.h
@@ -20,9 +20,6 @@
 
 #else
 
-/* POSIX requires at least 16 as a maximum iovcnt. */
-#define IOV_MAX 16
-
 /* Define our own POSIX-compatible iovec struct. */
 struct iovec
 {
@@ -30,6 +27,11 @@ struct iovec
 	size_t		iov_len;
 };
 
+#endif
+
+/* POSIX requires at least 16 as a maximum iovcnt. */
+#ifndef IOV_MAX
+#define IOV_MAX 16
 #endif
 
 /* Define a reasonable maximum that is safe to use on the stack. */
