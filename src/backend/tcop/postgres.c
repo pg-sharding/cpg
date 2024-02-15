899ce625d58dfc18c9446741c3f7beb8f248a9b9
Alexander Korotkov <akorotkov@postgresql.org>
2024-02-15 23:34:11 +0200

    Introduce transaction_timeout
    This commit adds timeout that is expected to be used as a prevention
    of long-running queries. Any session within the transaction will be
    terminated after spanning longer than this timeout.
    
    However, this timeout is not applied to prepared transactions.
    Only transactions with user connections are affected.
    
    Don't deal with transaction timeout in PostgresMain().  Instead, release
    transaction timeout activated by StartTransaction() in
    CommitTransaction()/AbortTransaction()/PrepareTransaction().  Deal with both
    enabling and disabling transaction timeout in assign_transaction_timeout().
    
    Also, remove potentially flaky timeouts-long isolation test, which has no
    guarantees to pass on slow/busy machines.
    
    Discussion: https://postgr.es/m/20240215230856.pc6k57tqxt7fhldm%40awork3.anarazel.de
    Discussion: https://postgr.es/m/CAAhFRxiQsRs2Eq5kCo9nXE3HTugsAAJdSQSmxncivebAxdmBjQ%40mail.gmail.com
    Author: Andrey Borodin <amborodin@acm.org>
    Author: Japin Li <japinli@hotmail.com>
    Author: Junwang Zhao <zhjwpku@gmail.com>
    
    Reviewed-by: Andres Freund <andres@anarazel.de>
    Reviewed-by: Fujii Masao <masao.fujii@oss.nttdata.com>
    Reviewed-by: bt23nguyent <bt23nguyent@oss.nttdata.com>
    Reviewed-by: Yuhang Qiu <iamqyh@gmail.com>
    
    Backport-by: rkhapov <r.khapov@ya.ru>
    Reviewed-by: reshke <reshke@double.cloud>
    
    =====
    Cherry-pick source: 51efe38cb92f4b15b68811bcce9ab878fbc71ea5 bf82f43790a675dd1b9522a7799357e61e7aa635

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -3372,6 +3372,17 @@ ProcessInterrupts(void)
 			IdleInTransactionSessionTimeoutPending = false;
 	}
 
+	if (TransactionTimeoutPending)
+	{
+		/* As above, ignore the signal if the GUC has been reset to zero. */
+		if (TransactionTimeout > 0)
+			ereport(FATAL,
+					(errcode(ERRCODE_TRANSACTION_TIMEOUT),
+					 errmsg("terminating connection due to transaction timeout")));
+		else
+			TransactionTimeoutPending = false;
+	}
+
 	if (IdleSessionTimeoutPending)
 	{
 		/* As above, ignore the signal if the GUC has been reset to zero. */
@@ -4515,7 +4526,8 @@ PostgresMain(const char *dbname, const char *username)
 				pgstat_report_activity(STATE_IDLEINTRANSACTION_ABORTED, NULL);
 
 				/* Start the idle-in-transaction timer */
-				if (IdleInTransactionSessionTimeout > 0)
+				if (IdleInTransactionSessionTimeout > 0
+					&& (IdleInTransactionSessionTimeout < TransactionTimeout || TransactionTimeout == 0))
 				{
 					idle_in_transaction_timeout_enabled = true;
 					enable_timeout_after(IDLE_IN_TRANSACTION_SESSION_TIMEOUT,
@@ -4528,7 +4540,8 @@ PostgresMain(const char *dbname, const char *username)
 				pgstat_report_activity(STATE_IDLEINTRANSACTION, NULL);
 
 				/* Start the idle-in-transaction timer */
-				if (IdleInTransactionSessionTimeout > 0)
+				if (IdleInTransactionSessionTimeout > 0
+					&& (IdleInTransactionSessionTimeout < TransactionTimeout || TransactionTimeout == 0))
 				{
 					idle_in_transaction_timeout_enabled = true;
 					enable_timeout_after(IDLE_IN_TRANSACTION_SESSION_TIMEOUT,
@@ -5126,7 +5139,8 @@ enable_statement_timeout(void)
 	/* must be within an xact */
 	Assert(xact_started);
 
-	if (StatementTimeout > 0)
+	if (StatementTimeout > 0
+		&& (StatementTimeout < TransactionTimeout || TransactionTimeout == 0))
 	{
 		if (!get_timeout_active(STATEMENT_TIMEOUT))
 			enable_timeout_after(STATEMENT_TIMEOUT, StatementTimeout);
