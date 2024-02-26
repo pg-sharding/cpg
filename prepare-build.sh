bba1e50d15a60562794bbbc51cf3806743619abd
Kirill Reshke <reshke@qavm-ac96707f.qemu>
2024-02-27 00:29:43 +0300

    Yandex build infra commit

---
+++ prepare-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export PACKAGE_NAME=postgresql-18
+export BUILD_USER=mdb-cc
+export VERSION=$(grep 'PACKAGE_VERSION=' configure | cut -d= -f2 | sed s/\'//g)-201-yandex.$(git rev-list HEAD --count).$(git rev-parse --short HEAD)
+export LC_ALL=C
+
+cat > debian/changelog<<EOH
+${PACKAGE_NAME} ($VERSION) stable; urgency=low
+
+  * Yandex autobuild
+
+ -- ${BUILD_USER} <${BUILD_USER}@yandex-team.ru>  $(date +'%a, %d %b %Y %H:%M:%S %z')
+EOH
+
+echo "VERSION=$VERSION" > version.properties
+
