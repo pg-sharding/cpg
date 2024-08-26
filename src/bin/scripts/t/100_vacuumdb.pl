37404703873eb787099f19471156e307a4e98bb3
diphantxm <diphantxm@yandex-team.ru>
2024-08-26 16:17:08 +0300

    Support FORCE option in analyze command
    Add new parameter to VACUUM command, FORCE, meaning VACUUM should
    terminate all backends that prevents the execution by holding conflicting lock
    
    fix

--- src/bin/scripts/t/100_vacuumdb.pl
+++ src/bin/scripts/t/100_vacuumdb.pl
@@ -48,6 +48,10 @@ $node->issues_sql_like(
 	[ 'vacuumdb', '--skip-locked', '--analyze-only', 'postgres' ],
 	qr/statement: ANALYZE \(SKIP_LOCKED\).*;/,
 	'vacuumdb --skip-locked --analyze-only');
+$node->issues_sql_like(
+	[ 'vacuumdb', '--force', '--analyze-only', 'postgres' ],
+	qr/statement: ANALYZE \(FORCE\).*;/,
+	'vacuumdb --force --analyze-only');
 $node->command_fails(
 	[ 'vacuumdb', '--analyze-only', '--disable-page-skipping', 'postgres' ],
 	'--analyze-only and --disable-page-skipping specified together');
