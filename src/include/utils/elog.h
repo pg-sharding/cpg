2e2f0a615d4b05b44fc5e49c316b4b91add1e753
diphantxm <diphantxm@yandex-team.ru>
2024-09-09 16:50:15 +0300

    parameter max_log_size to truncate logs
    There is no need to log the entire query, because it may be large and take lots of space on disk. Parameter max_log_size set the maximum length for logged query. Everything beyond that length is truncated. Value 0 disables the parameter.

--- src/include/utils/elog.h
+++ src/include/utils/elog.h
@@ -430,6 +430,7 @@ extern int	Log_destination;
 extern char *Log_destination_string;
 extern bool syslog_sequence_numbers;
 extern bool syslog_split_messages;
+extern PGDLLIMPORT int max_log_size;
 
 /* Log destination bitmap */
 #define LOG_DESTINATION_STDERR	 1
