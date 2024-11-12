99b48270c34963d31cee774b77c615c809c9cbdf
diphantxm <diphantxm@yandex-team.ru>
2024-11-12 15:45:28 +0300

    truncate query to be logged in simple query

--- src/include/utils/elog.h
+++ src/include/utils/elog.h
@@ -442,6 +442,7 @@ extern PGDLLIMPORT int max_log_size;
 extern void DebugFileOpen(void);
 extern char *unpack_sql_state(int sql_state);
 extern bool in_error_recursion_trouble(void);
+char* build_query_log(const char* query, bool *copied);
 
 #ifdef HAVE_SYSLOG
 extern void set_syslog_parameters(const char *ident, int facility);
