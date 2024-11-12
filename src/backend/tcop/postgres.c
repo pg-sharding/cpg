461b724c7b81ddd5b29f65cd0f6173b5d140dc12
diphantxm <diphantxm@yandex-team.ru>
2024-11-12 15:45:28 +0300

    truncate query to be logged in simple query

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -966,11 +966,14 @@ exec_simple_query(const char *query_string)
 	bool		was_logged = false;
 	bool		use_implicit_block;
 	char		msec_str[32];
+	char* 		query_log;
 
 	/*
 	 * Report query to various monitoring facilities.
 	 */
 	debug_query_string = query_string;
+	bool copied = false;
+	query_log = build_query_log(query_string, &copied);
 
 	pgstat_report_activity(STATE_RUNNING, query_string);
 
@@ -1015,7 +1018,7 @@ exec_simple_query(const char *query_string)
 	if (check_log_statement(parsetree_list))
 	{
 		ereport(LOG,
-				(errmsg("statement: %s", query_string),
+				(errmsg("statement: %s", query_log),
 				 errhidestmt(true),
 				 errdetail_execute(parsetree_list)));
 		was_logged = true;
@@ -1311,7 +1314,7 @@ exec_simple_query(const char *query_string)
 		case 2:
 			ereport(LOG,
 					(errmsg("duration: %s ms  statement: %s",
-							msec_str, query_string),
+							msec_str, query_log),
 					 errhidestmt(true),
 					 errdetail_execute(parsetree_list)));
 			break;
@@ -1322,6 +1325,8 @@ exec_simple_query(const char *query_string)
 
 	TRACE_POSTGRESQL_QUERY_DONE(query_string);
 
+	if (query_log && copied)
+		pfree(query_log);
 	debug_query_string = NULL;
 }
 
