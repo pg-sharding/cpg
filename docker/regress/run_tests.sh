46a87014807cb5e662f809185425e81b97a26041
Yury Frolov <57130330+EinKrebs@users.noreply.github.com>
2025-01-24 16:00:05 +0500

    Rewrite Dockerfile logic

--- docker/regress/run_tests.sh
+++ docker/regress/run_tests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
 set -ex
 
-cd /home/build-user
-
-CFLAGS="" ./configure --prefix=/home/build-user/pgbin  --without-mdblocales  --enable-depend --enable-cassert --enable-debug --enable-tap-tests
-
-make -j8
 sed -i  '/mdb-related/,$d' src/test/regress/expected/misc.out src/test/regress/sql/misc.sql
 
 make check
