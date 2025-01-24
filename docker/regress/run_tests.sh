16af77bb97163bf84de89438695af0424665cd6e
Yury Frolov <yura200253@gmail.com>
2025-01-24 11:08:03 +0500

    Rework docker image logic

--- docker/regress/run_tests.sh
+++ docker/regress/run_tests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
 set -ex
 
-cd /home/build-user
-
-CFLAGS="" ./configure --prefix=/home/build-user/pgbin  --without-mdblocales  --enable-depend --enable-cassert --enable-debug --enable-tap-tests
-
-make -j8
 sed -i  '/mdb-related/,$d' src/test/regress/expected/misc.out src/test/regress/output/misc.source src/test/regress/input/misc.source
 
 make check
