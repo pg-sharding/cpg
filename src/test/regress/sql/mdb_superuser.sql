f4ea530a8f511371a24c4c74c120ce28fd3ce66a
reshke <reshkekirill@gmail.com>
2025-02-07 13:07:37 +0000

    Introduce mdb_read_all_data/mdb_write_all_data

--- src/test/regress/sql/mdb_superuser.sql
+++ src/test/regress/sql/mdb_superuser.sql
@@ -7,8 +7,6 @@ CREATE ROLE regress_mdb_su_role_o2;
 
 GRANT mdb_superuser TO regress_mdb_su_role_o1;
 
-GRANT mdb_admin TO mdb_superuser;
-
 CREATE ROLE regress_superuser WITH SUPERUSER;
 
 GRANT mdb_superuser TO regress_mdb_superuser_user1;
@@ -79,7 +77,7 @@ DROP TABLE regress_pgrsf_table;
 DROP TABLE regress_pgwsf_table;
 DROP TABLE regress_pgxsp_table;
 
--- does allowed to creare database, role or extension
+-- does NOT allowed to create database, role or extension
 -- or grant such priviledge 
 
 CREATE DATABASE regress_db_fail;
@@ -157,7 +155,6 @@ DROP ROLE regress_mdb_su_role_o2;
 DROP VIEW regress_mdb_superuser_view;
 DROP FUNCTION regress_mdb_superuser_add;
 DROP TABLE regress_mdb_superuser_schema.regress_mdb_superuser_table;
-DROP TABLE regress_mdb_superuser_table;
 DROP SCHEMA regress_mdb_superuser_schema;
 DROP ROLE regress_mdb_superuser_user1;
 DROP ROLE regress_mdb_superuser_user2;
@@ -166,3 +163,4 @@ DROP TABLE regress_superuser_table;
 DROP TABLE regress_pgrsf_table;
 DROP TABLE regress_pgwsf_table;
 DROP TABLE regress_pgxsp_table;
+DROP ROLE regress_superuser;
