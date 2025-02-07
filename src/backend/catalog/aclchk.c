9aa2220489580d684f7f3034e0bbb494df4be422
reshke <reshkekirill@gmail.com>
2025-02-07 13:07:37 +0000

    Introduce mdb_read_all_data/mdb_write_all_data

--- src/backend/catalog/aclchk.c
+++ src/backend/catalog/aclchk.c
@@ -3288,6 +3288,8 @@ pg_class_aclmask_ext(Oid table_oid, Oid roleid, AclMode mask,
 	bool		isNull;
 	Acl		   *acl;
 	Oid			ownerId;
+	Oid			mdb_read_all_data_oid;
+	Oid			mdb_write_all_data_oid;
 
 	/*
 	 * Must get the relation's tuple from pg_class
@@ -3338,6 +3340,9 @@ pg_class_aclmask_ext(Oid table_oid, Oid roleid, AclMode mask,
 	 */
 	ownerId = classForm->relowner;
 
+	mdb_read_all_data_oid = get_role_oid("mdb_read_all_data", true);
+	mdb_write_all_data_oid = get_role_oid("mdb_write_all_data", true);
+
 	aclDatum = SysCacheGetAttr(RELOID, tuple, Anum_pg_class_relacl,
 							   &isNull);
 	if (isNull)
@@ -3377,6 +3382,17 @@ pg_class_aclmask_ext(Oid table_oid, Oid roleid, AclMode mask,
 		has_privs_of_role(roleid, ROLE_PG_READ_ALL_DATA))
 		result |= ACL_SELECT;
 
+
+	/*
+	 * Check if ACL_SELECT is being checked and, if so, and not set already as
+	 * part of the result, then check if the user is a member of the
+	 * mdb_read_all_data role, and this is not some dangerous relation to grant SELECT to
+	 */
+	if (mask & ACL_SELECT && !(result & ACL_SELECT) &&
+		has_privs_of_role(roleid, mdb_read_all_data_oid) && 
+		!has_privs_of_unwanted_system_role(ownerId))
+		result |= ACL_SELECT;
+
 	/*
 	 * Check if ACL_INSERT, ACL_UPDATE, or ACL_DELETE is being checked and, if
 	 * so, and not set already as part of the result, then check if the user
@@ -3387,6 +3403,18 @@ pg_class_aclmask_ext(Oid table_oid, Oid roleid, AclMode mask,
 	if (mask & (ACL_INSERT | ACL_UPDATE | ACL_DELETE) &&
 		!(result & (ACL_INSERT | ACL_UPDATE | ACL_DELETE)) &&
 		has_privs_of_role(roleid, ROLE_PG_WRITE_ALL_DATA))
+		result |= (mask & (ACL_INSERT | ACL_UPDATE | ACL_DELETE));
+
+	/*
+	 * Check if ACL_INSERT, ACL_UPDATE, or ACL_DELETE is being checked and, if
+	 * so, and not set already as part of the result, then check if the user
+	 * is a member of the mdb_write_all_data role, and this is not some 
+	 * dangerous relation to grant write access.
+	 */
+	if (mask & (ACL_INSERT | ACL_UPDATE | ACL_DELETE) &&
+		!(result & (ACL_INSERT | ACL_UPDATE | ACL_DELETE)) &&
+		has_privs_of_role(roleid, mdb_write_all_data_oid) &&
+		!has_privs_of_unwanted_system_role(ownerId))
 		result |= (mask & (ACL_INSERT | ACL_UPDATE | ACL_DELETE));
 
 	return result;
