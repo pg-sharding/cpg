869dbfe6205e169097097c9da6aaf8e4bc35066e
reshke <reshke@double.cloud>
2026-01-18 14:12:21 +0000

    v6 of bt_page_items pretty-print

---
+++ contrib/pageinspect/pageinspect--1.11--1.12.sql
@@ -0,0 +1,21 @@
+/* contrib/pageinspect/pageinspect--1.11--1.12.sql */
+
+-- complain if script is sourced in psql, rather than via ALTER EXTENSION
+\echo Use "ALTER EXTENSION pageinspect UPDATE TO '1.12'" to load this file. \quit
+
+
+DROP FUNCTION bt_page_items(text, int8);
+CREATE FUNCTION bt_page_items(IN relname text, IN blkno int8,
+    IN pretty_print boolean DEFAULT FALSE,
+    OUT itemoffset smallint,
+    OUT ctid tid,
+    OUT itemlen smallint,
+    OUT nulls bool,
+    OUT vars bool,
+    OUT data text,
+    OUT dead boolean,
+    OUT htid tid,
+    OUT tids tid[])
+RETURNS SETOF record
+AS 'MODULE_PATHNAME', 'bt_page_items_1_9'
+LANGUAGE C STRICT PARALLEL SAFE;
