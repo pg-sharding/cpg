478d45ae02c5e89ce2247dca89a29362562391d0
reshke <reshke@qavm-273b4667.qemu>
2026-07-13 19:14:59 +0300

    Keepgoing fix

--- contrib/amcheck/verify_nbtree.c
+++ contrib/amcheck/verify_nbtree.c
@@ -81,6 +81,8 @@ typedef struct BtreeCheckState
 	bool		heapallindexed;
 	/* Also verify that our index tuples point to non-HOT heap tuples */
 	bool		checkhot;
+	/* Do not give up after first corruption */
+	bool 		keepgoing;
 	/* Also making sure non-pivot tuples can be found by new search? */
 	bool		rootdescend;
 	/* Per-page context */
@@ -142,12 +144,12 @@ PG_FUNCTION_INFO_V1(bt_index_check);
 PG_FUNCTION_INFO_V1(bt_index_parent_check);
 
 static void bt_index_check_internal(Oid indrelid, bool parentcheck,
-									bool heapallindexed, bool rootdescend, bool checkhot);
+									bool heapallindexed, bool rootdescend, bool checkhot, bool keepgoing);
 static inline void btree_index_checkable(Relation rel);
 static inline bool btree_index_mainfork_expected(Relation rel);
 static void bt_check_every_level(Relation rel, Relation heaprel,
 								 bool heapkeyspace, bool readonly, bool heapallindexed,
-								 bool checkhot, bool rootdescend);
+								 bool checkhot, bool rootdescend, bool keepgoing);
 static BtreeLevel bt_check_level_from_leftmost(BtreeCheckState *state,
 											   BtreeLevel level);
 static bool bt_leftmost_ignoring_half_dead(BtreeCheckState *state,
@@ -215,6 +217,7 @@ bt_index_check(PG_FUNCTION_ARGS)
 	Oid			indrelid = PG_GETARG_OID(0);
 	bool		heapallindexed = false;
 	bool		checkhot = false;
+	bool		keepgoing = false;
 
 	if (PG_NARGS() >= 2)
 		heapallindexed = PG_GETARG_BOOL(1);
@@ -222,7 +225,10 @@ bt_index_check(PG_FUNCTION_ARGS)
 	if (PG_NARGS() >= 3)
 		checkhot = PG_GETARG_BOOL(2);
 
-	bt_index_check_internal(indrelid, false, heapallindexed, false, checkhot);
+	if (PG_NARGS() >= 4)
+		keepgoing = PG_GETARG_BOOL(3);
+
+	bt_index_check_internal(indrelid, false, heapallindexed, false, checkhot, keepgoing);
 
 	PG_RETURN_VOID();
 }
@@ -243,16 +249,21 @@ bt_index_parent_check(PG_FUNCTION_ARGS)
 	bool		heapallindexed = false;
 	bool		rootdescend = false;
 	bool		checkhot = false;
+	bool		keepgoing = false;
 
 	if (PG_NARGS() >= 2)
 		heapallindexed = PG_GETARG_BOOL(1);
+
 	if (PG_NARGS() >= 3)
 		rootdescend = PG_GETARG_BOOL(2);
 
 	if (PG_NARGS() >= 4)
 		checkhot = PG_GETARG_BOOL(3);
 
-	bt_index_check_internal(indrelid, true, heapallindexed, rootdescend, checkhot);
+	if (PG_NARGS() >= 5)
+		keepgoing = PG_GETARG_BOOL(4);
+
+	bt_index_check_internal(indrelid, true, heapallindexed, rootdescend, checkhot, keepgoing);
 
 	PG_RETURN_VOID();
 }
@@ -262,7 +273,7 @@ bt_index_parent_check(PG_FUNCTION_ARGS)
  */
 static void
 bt_index_check_internal(Oid indrelid, bool parentcheck, bool heapallindexed,
-						bool rootdescend,  bool checkhot)
+						bool rootdescend,  bool checkhot, bool keepgoing)
 {
 	Oid			heapid;
 	Relation	indrel;
@@ -373,7 +384,7 @@ bt_index_check_internal(Oid indrelid, bool parentcheck, bool heapallindexed,
 
 		/* Check index, possibly against table it is an index on */
 		bt_check_every_level(indrel, heaprel, heapkeyspace, parentcheck,
-							 heapallindexed,  checkhot, rootdescend);
+							 heapallindexed,  checkhot, rootdescend, keepgoing);
 	}
 
 	/* Roll back any GUC changes executed by index functions */
@@ -474,7 +485,7 @@ btree_index_mainfork_expected(Relation rel)
  */
 static void
 bt_check_every_level(Relation rel, Relation heaprel, bool heapkeyspace,
-					 bool readonly, bool heapallindexed, bool checkhot, bool rootdescend)
+					 bool readonly, bool heapallindexed, bool checkhot, bool rootdescend, bool keepgoing)
 {
 	BtreeCheckState *state;
 	Snapshot	snapshot = InvalidSnapshot;
@@ -507,6 +518,7 @@ bt_check_every_level(Relation rel, Relation heaprel, bool heapkeyspace,
 	state->heapallindexed = heapallindexed;
 	state->checkhot = checkhot;
 	state->rootdescend = rootdescend;
+	state->keepgoing = keepgoing;
 
 	if (state->heapallindexed || state->checkhot)
 	{
@@ -1174,7 +1186,7 @@ bt_target_page_check(BtreeCheckState *state)
 	topaque = BTPageGetOpaque(state->target);
 	max = PageGetMaxOffsetNumber(state->target);
 
-	keepgoing = true; // state->keepgoing;
+	keepgoing = state->keepgoing;
 
 	elog(DEBUG2, "verifying %u items on %s block %u", max,
 		 P_ISLEAF(topaque) ? "leaf" : "internal", state->targetblock);
@@ -2251,11 +2263,13 @@ bt_child_check(BtreeCheckState *state, BTScanInsert targetkey,
 	Page		child;
 	BTPageOpaque copaque;
 	BTPageOpaque topaque;
+	bool		keepgoing;
 
 	itemid = PageGetItemIdCareful(state, state->targetblock,
 								  state->target, downlinkoffnum);
 	itup = (IndexTuple) PageGetItem(state->target, itemid);
 	childblock = BTreeTupleGetDownLink(itup);
+	keepgoing = state->keepgoing;
 
 	/*
 	 * Caller must have ShareLock on target relation, because of
@@ -2338,7 +2352,7 @@ bt_child_check(BtreeCheckState *state, BTScanInsert targetkey,
 	 * to test.
 	 */
 	if (P_ISDELETED(copaque))
-		ereport(ERROR,
+		ereport(keepgoing ? WARNING : ERROR,
 				(errcode(ERRCODE_INDEX_CORRUPTED),
 				 errmsg("downlink to deleted page found in index \"%s\"",
 						RelationGetRelationName(state->rel)),
@@ -2379,7 +2393,7 @@ bt_child_check(BtreeCheckState *state, BTScanInsert targetkey,
 
 		if (!invariant_l_nontarget_offset(state, targetkey, childblock, child,
 										  offset))
-			ereport(ERROR,
+			ereport(keepgoing ? WARNING : ERROR,
 					(errcode(ERRCODE_INDEX_CORRUPTED),
 					 errmsg("down-link lower bound invariant violated for index \"%s\"",
 							RelationGetRelationName(state->rel)),
