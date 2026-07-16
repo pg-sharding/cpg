97d7bd5751acbb8a8d7a1e94fe6cc960e847aac8
reshke <reshke@qavm-273b4667.qemu>
2026-07-16 11:53:24 +0300

    Add warning for pg_amcheck rootdescend and parent-check modes

--- src/bin/pg_amcheck/pg_amcheck.c
+++ src/bin/pg_amcheck/pg_amcheck.c
@@ -423,6 +423,7 @@ main(int argc, char *argv[])
 			case 9:
 				opts.rootdescend = true;
 				opts.parent_check = true;
+				pg_log_warning("Rootdescend blocks concurrent data modification. It may also cause corruption checking to take considerably longer and consume considerably more resources on the server.");
 				break;
 			case 10:
 				opts.strict_names = false;
@@ -432,6 +433,7 @@ main(int argc, char *argv[])
 				break;
 			case 12:
 				opts.parent_check = true;
+				pg_log_warning("Parent check blocks concurrent data modification. It may also cause corruption checking to take considerably longer and consume considerably more resources on the server.");
 				break;
 			case 13:
 				opts.install_missing = true;
