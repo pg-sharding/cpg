01eaaec44dfb9dac1840f3a8de5a3cc1cc63e1eb
reshke <reshke@qavm-273b4667.qemu>
2026-08-11 18:30:52 +0300

    Hide WAL internals from non-superuser in error report
    Because of non-backend users if this (waldump), we have to check for
    issuperuser without catcache. Simpy delete all WAL details from error
    report since them dont seem to severe for thoubles they make

--- src/backend/access/transam/xlogreader.c
+++ src/backend/access/transam/xlogreader.c
@@ -1143,16 +1143,13 @@ ValidXLogRecordHeader(XLogReaderState *state, XLogRecPtr RecPtr,
 	if (record->xl_tot_len < SizeOfXLogRecord)
 	{
 		report_invalid_record(state,
-							  "invalid record length at %X/%08X: expected at least %u, got %u",
-							  LSN_FORMAT_ARGS(RecPtr),
-							  (uint32) SizeOfXLogRecord, record->xl_tot_len);
+										"invalid record");
 		return false;
 	}
 	if (!RmgrIdIsValid(record->xl_rmid))
 	{
 		report_invalid_record(state,
-							  "invalid resource manager ID %u at %X/%08X",
-							  record->xl_rmid, LSN_FORMAT_ARGS(RecPtr));
+										"invalid record");
 		return false;
 	}
 	if (randAccess)
@@ -1164,9 +1161,7 @@ ValidXLogRecordHeader(XLogReaderState *state, XLogRecPtr RecPtr,
 		if (!(record->xl_prev < RecPtr))
 		{
 			report_invalid_record(state,
-								  "record with incorrect prev-link %X/%08X at %X/%08X",
-								  LSN_FORMAT_ARGS(record->xl_prev),
-								  LSN_FORMAT_ARGS(RecPtr));
+											"invalid record");
 			return false;
 		}
 	}
@@ -1180,9 +1175,7 @@ ValidXLogRecordHeader(XLogReaderState *state, XLogRecPtr RecPtr,
 		if (record->xl_prev != PrevRecPtr)
 		{
 			report_invalid_record(state,
-								  "record with incorrect prev-link %X/%08X at %X/%08X",
-								  LSN_FORMAT_ARGS(record->xl_prev),
-								  LSN_FORMAT_ARGS(RecPtr));
+											"invalid record");
 			return false;
 		}
 	}
