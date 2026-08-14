416048737348680301dd5e8ecc85c73194b7b004
reshke <reshke@double.cloud>
2026-08-14 07:38:18 +0000

    mdb_replication fix

--- src/backend/replication/walsender.c
+++ src/backend/replication/walsender.c
@@ -846,6 +846,8 @@ StartReplication(StartReplicationCmd *cmd)
 	XLogRecPtr	FlushPtr;
 	TimeLineID	FlushTLI;
 
+	check_permissions();
+
 	/* create xlogreader for physical replication */
 	xlogreader =
 		XLogReaderAllocate(wal_segment_size, NULL,
